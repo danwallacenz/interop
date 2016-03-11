c9f1a9f367faedcad3439d9e49495a30e67529f3
Dan Wallace <daniel.wallace@ikegps.com>
2016-03-11 13:47:49 +1300

    create LegacyViewController

---
+++ interop/LegacyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LegacyViewController.swift
+//  interop
+//
+//  Created by Daniel Wallace on 11/03/16.
+//  Copyright © 2016 IkeGPS. All rights reserved.
+//
+
+extension LegacyViewController {
+    
+    func showIconViewWithIcon(icon: Icon) {
+        switch icon {
+        case let .Image(image):
+            self.showIconViewWithImage(image)
+        case let .Color(color):
+            self.showIconViewWithColor(color)
+        }
+    }
+}
+
+
